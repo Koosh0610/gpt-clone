0c22bef7a2084dc6d91b016d14fbd4bfa21dccdf
Kush Juvekar <119339573+Koosh0610@users.noreply.github.com>
2025-01-27 13:03:36 +0530

    Update main.py
    changes to pydantic model

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 # Initialize your models and clients here
-llm = Groq(model="llama3-70b-8192", temperature=0)
+llm = Groq(model="deepseek-r1-distill-llama-70b", temperature=0)
 
 
 #Defining Pydantic Models
+    
 class ChatRequest(BaseModel):
     prompt: str
     message_history: List[ChatMessage]
@@ -37,21 +38,21 @@ class ChatResponse(BaseModel):
 @app.post("/chat", response_model=ChatResponse)
 async def chat(request: ChatRequest):
     try:
-        print(agent_prompt.format(input_text=request.prompt,message_history=request.message_history,user_info=request.user_info))
         response = llm.complete(agent_prompt.format(input_text=request.prompt,message_history=request.message_history,user_info=request.user_info))
         return ChatResponse(response=response.text)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
 agent_prompt = """
-You are a helpful agent that answers user queries while also looking the conversation history. You will also be provided with certain information regarding the user that may OR may not help you tailor responses to the user.
-User Info:
-{user_info}
-History:
-{message_history}
-Query:
-{input_text}
-"""
+    <description>
+        You are a helpful agent that answers user queries while also considering the conversation history. 
+        You will also be provided with certain information regarding the user that may or may not help you tailor responses.
+    </description>
+    <context>
+        <user_info>{user_info}</user_info>
+        <history>{message_history}</history>
+    </context>
+    <query>{input_text}</query>"""
 
 if __name__ == "__main__":
     import uvicorn
